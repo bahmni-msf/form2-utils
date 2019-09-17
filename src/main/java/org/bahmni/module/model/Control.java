71802d68f99a93e52d5e60583ab5c0420d845df8
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-09-17 16:25:22 +0530

    Siva, Vinisha | OB-321 | Add form2 reader service

---
+++ src/main/java/org/bahmni/module/model/Control.java
@@ -0,0 +1,53 @@
+package org.bahmni.module.model;
+
+import java.util.List;
+
+public class Control {
+
+    private ControlProperties properties;
+    private Concept concept;
+    private ControlLabel label;
+    private List<Control> controls;
+    private String type;
+
+    public ControlLabel getLabel() {
+        return label;
+    }
+
+    public void setLabel(ControlLabel label) {
+        this.label = label;
+    }
+
+    public List<Control> getControls() {
+        return controls;
+    }
+
+    public void setControls(List<Control> controls) {
+        this.controls = controls;
+    }
+
+    public ControlProperties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(ControlProperties properties) {
+        this.properties = properties;
+    }
+
+    public Concept getConcept() {
+        return concept;
+    }
+
+    public void setConcept(Concept concept) {
+        this.concept = concept;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
